36113abca630573ae6d58d19cc5c3e6bf2ec88a5
Tim Eaton <tim@yakdrive.io>
2025-08-02 15:48:23 +0200

    Added sanity integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,33 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sanityIntegration from "@sanity/astro";
+import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
+import { loadEnv } from "vite";
+import netlify from "@astrojs/netlify";
 
-// https://astro.build/config
+const { PUBLIC_SANITY_STUDIO_PROJECT_ID, PUBLIC_SANITY_STUDIO_DATASET } =
+  loadEnv(process.env.NODE_ENV || "development", process.cwd(), "");
+
 export default defineConfig({
-  integrations: [mdx()],
+  output: "server",
+  integrations: [
+    sanityIntegration({
+      projectId: PUBLIC_SANITY_STUDIO_PROJECT_ID,
+      dataset: PUBLIC_SANITY_STUDIO_DATASET,
+      useCdn: false,
+      studioBasePath: "/admin",
+    }),
+    react(),
+    mdx(),
+  ],
+  adapter: netlify(),
+  vite: {
+    ssr: {
+      external: ["styled-components"],
+    },
+    optimizeDeps: {
+      include: ["styled-components"],
+    },
+  },
 });
